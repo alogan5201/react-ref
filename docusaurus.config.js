6cd820eea7f9f7493ddf92e97df37b6834c6db20
alogan5201 <andrewthomaslogan@gmail.com>
2022-06-26 17:29:08 -0400

    up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'Dinosaurs are cool',
   url: 'https://www.react.andrewlogan.dev',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
